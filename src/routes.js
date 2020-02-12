bb769d88cd0c5df1542dd26dcb602eefa047e0c6
Leonardo Sampaio da Cruz <leonardosamp11@gmail.com>
2020-02-12 18:33:00 -0300

    recipients routes and provider middleware

--- src/routes.js
+++ src/routes.js
@@ -2,16 +2,23 @@ import { Router } from 'express';
 
 import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
+import RecipientsController from './app/controllers/RecipientsController';
 
-import authMiddleware from './app/middlewares/auth';
+import auth from './app/middlewares/auth';
 
 const routes = new Router();
 
 routes.post('/users', UserController.store);
 routes.post('/sessions', SessionController.store);
 
-routes.use(authMiddleware);
+routes.use(auth.authMiddleware);
 
 routes.put('/users', UserController.update);
+
+routes.use(auth.providerMiddleware);
+
+routes.post('/recipients', RecipientsController.store);
+
+routes.put('/recipients/:id', RecipientsController.update);
 
 export default routes;
