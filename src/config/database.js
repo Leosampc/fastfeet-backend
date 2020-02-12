e2972ba5907b86813dc071db119e19dcfe3cff21
Leonardo Sampaio da Cruz <leonardosamp11@gmail.com>
2020-02-12 11:44:29 -0300

    db connection config

---
+++ src/config/database.js
@@ -0,0 +1,12 @@
+export default {
+    dialect: 'postgres',
+    host: 'localhost',
+    username: 'postgres',
+    password: 'docker',
+    database: 'fastfeet',
+    define: {
+        timestamp: true,
+        underscored: true,
+        underscoredAll: true,
+    },
+};
